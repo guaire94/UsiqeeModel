a64666e3769e1d5d0db65f2709893e980988c548
Quentin Gallois <guaire94.perso@gmail.com>
2021-04-22 09:14:22 +0200

    simple class test

---
+++ UsiqeeModel/Model/Admin.swift
@@ -0,0 +1,30 @@
+//
+//  User.swift
+//  UsiqeeAdmin
+//
+//  Created by Quentin Gallois on 21/10/2020.
+//
+//
+//import Firebase
+//import FirebaseFirestoreSwift
+//
+//struct Admin: Identifiable, Codable {
+//    @DocumentID var id: String?
+//    var mail: String
+//    var createdDate: Timestamp
+//}
+
+
+public final class SwiftyLib {
+
+    let name = "SwiftyLib"
+    
+    public func add(a: Int, b: Int) -> Int {
+        return a + b
+    }
+    
+    public func sub(a: Int, b: Int) -> Int {
+        return a - b
+    }
+    
+}
