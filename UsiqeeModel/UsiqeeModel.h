05f212a4b6c15cdb5a4bcbd4ccac676be59bbe22
Quentin Gallois <guaire94.perso@gmail.com>
2021-04-22 09:11:40 +0200

    first commit

---
+++ UsiqeeModel/UsiqeeModel.h
@@ -0,0 +1,18 @@
+//
+//  UsiqeeModel.h
+//  UsiqeeModel
+//
+//  Created by Guaire94 on 22/04/2021.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for UsiqeeModel.
+FOUNDATION_EXPORT double UsiqeeModelVersionNumber;
+
+//! Project version string for UsiqeeModel.
+FOUNDATION_EXPORT const unsigned char UsiqeeModelVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <UsiqeeModel/PublicHeader.h>
+
+
